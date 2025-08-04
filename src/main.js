1d7e554e1d163f52ed85e104b52e6f91ff4f191b
ongsoony8382 <ongsoony8382@gmail.com>
2025-08-04 10:56:23 +0900

    pinia-plugin-persistedstate 설치 및 세팅

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia() 
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 app.use(router)
 
 router.isReady().then(()=>{ //프로미스 객체의 주소값을 리턴 
