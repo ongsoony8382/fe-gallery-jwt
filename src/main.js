6a6af27ecced097a5fd4f45bbf9f4cd30ee9ee5e
ongsoony8382 <ongsoony8382@gmail.com>
2025-08-04 11:02:01 +0900

    부트스트랩3 설치

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
+//부트스트랩
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 const app = createApp(App)
 
 const pinia = createPinia() 
@@ -14,6 +19,7 @@ pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
+app.use(BootstrapVue3)
 
 router.isReady().then(()=>{ //프로미스 객체의 주소값을 리턴 
     app.mount('#app')
